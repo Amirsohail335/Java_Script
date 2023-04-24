eb4ffcf36ed51239a8b767c6bcd146a30194e3a4
Amir Sohail <amirsohail335@gmail.com>
2023-04-24 11:02:36 +0530

    committed

---
+++ globalscope.js
@@ -0,0 +1,17 @@
+let name = "Amir"
+
+function greet(){
+    console.log("greetings !" , name);
+
+    function test(){
+        console.log("Jiii")
+    }
+
+}
+
+function fun(){
+    console.log("Have fun", name);
+}
+
+greet();
+fun();
