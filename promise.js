736647419c6e3de6b06ec956adb962ad2c27c412
Amir Sohail <amirsohail335@gmail.com>
2023-04-27 15:07:36 +0530

    committed

---
+++ promise.js
@@ -0,0 +1,12 @@
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Promise resolved!');
+    }, 1000);
+  });
+  
+  myPromise.then((result) => {
+    console.log(result);
+  }).catch((error) => {
+    console.error(error);
+  });
+  
