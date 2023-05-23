b183e4cd45c8b98343cf66fba26249b0488b56c2
Amir Sohail <amirsohail335@gmail.com>
2023-05-23 10:37:46 +0530

    committed

---
+++ hoisting.js
@@ -0,0 +1,6 @@
+let x = 4;
+console.log(x);
+
+
+y = 5;
+console.log(y);
---
+++ scopes/autoglobalscope.js
@@ -0,0 +1,16 @@
+var teacher = "Sanket";
+
+function fun(){
+    var teacher = "Pulkit";
+    console.log(teacher , teachingAssistant);
+    teachingAssistant = "Vaibhav"
+}
+
+function gun(){
+    console.log(subject);
+    var subject = "JS";
+    console.log(teacher);
+}
+
+console.log(teacher);
+fun();
