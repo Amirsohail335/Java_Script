cac1d6876965c33c9f39f384ce6d03626fca984a
Amirsohail335 <amirsohail335@gmail.com>
2023-07-11 07:40:17 +0530

    committed

---
+++ Node js/index.js
@@ -0,0 +1,5 @@
+console.log("Start");
+
+let arr = [5,4,2,7,1,3,5,6];
+
+let x = 5;
---
+++ demotest.js
@@ -0,0 +1,36 @@
+// let x = ["Edison", "Tesla", "Einstein"];
+
+// let [,a] = x;
+// document.write(a);
+
+// let states = []
+// states.push (['Rihanna', {
+
+//     empId : 'I10',
+//     city : 'ENG',
+//     age:25
+// }]);
+
+// console.log(...state)
+
+
+let n = "sam"
+function demo(){
+    n += "mark"
+}
+console.log(n)
+
+
+// const result = [];
+
+// function check(par1 , ...par2){
+//     return result.push(par1, par2)
+
+// }
+
+// console.log(check("Hello", "World"))
+
+let x = new Array([1,2,3])
+
+x.delete(1)
+console.log(x)
