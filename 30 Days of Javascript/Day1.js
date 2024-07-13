041d0131dd7aa337134911c6404dfa6c7b5e4dea
Amirsohail335 <amirsohail335@gmail.com>
2024-07-13 16:47:37 +0530

    Day 1 of 30 days of code

---
+++ 30 Days of Javascript/Day1.js
@@ -0,0 +1,14 @@
+//Activity 1 :- Variable Declaration
+//Task 1
+ var a = 333;
+ console.log(a)
+
+ //Task 2
+ let name = "Amir Sohail";
+ console.log(name);
+
+ //Activity 2 :-Constant Declaration
+
+//Task 3
+const isTrue = true;
+console.log(isTrue);
